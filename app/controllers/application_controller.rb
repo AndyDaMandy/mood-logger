05fd6e5b7bfdff0f37ab1e41f63633cb66b3aacd
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-08-01 15:15:11 -0400

    setting up routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     def after_sign_in_path_for(resource)
-        moods
+       
     end
     def after_sign_out_path_for(resource)
         root_path
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,6 +20,7 @@ Devise.setup do |config|
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
   config.navigational_formats = ['*/*', :html, :turbo_stream]
+  config.sign_out_via = :delete
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
